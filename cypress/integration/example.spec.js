413895b670a04da119c52a31a7258eae26673b96
ajoy267 <andrew.joy12@hotmail.com>
2021-10-05 16:25:10 -0700

    added a couple cypress tests to run

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -8,5 +8,11 @@ describe('Example Test', ()=>{
         it('contains body tag', ()=>{
             cy.get('body').should('be.visible');
         });
+        it('reveal answer button clicks', ()=>{
+            cy.get('#reveal-button').click();
+        });
+        it('contains title', ()=>{
+            cy.get('#title').should('be.visible');
+        });
     });
 });
